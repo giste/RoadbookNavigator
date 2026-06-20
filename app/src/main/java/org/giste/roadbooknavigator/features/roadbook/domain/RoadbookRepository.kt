6e16a3f40e04488271a95ea3124bf3336a17e0e8
Giste <giste.trappiste@gmail.com>
2026-06-20 19:35:29 +0200

    feat(roadbook): add RoadbookRepository interface to domain

---
+++ app/src/main/java/org/giste/roadbooknavigator/features/roadbook/domain/RoadbookRepository.kt
@@ -0,0 +1,34 @@
+/*
+ * Copyright (C) 2026  Giste
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.giste.roadbooknavigator.features.roadbook.domain
+
+import java.io.InputStream
+
+/**
+ * Interface defining the contract for roadbook data operations.
+ * Following Clean Architecture, this sits in the Domain layer.
+ */
+interface RoadbookRepository {
+    /**
+     * Loads and parses a roadbook from the provided input stream.
+     *
+     * @param inputStream The source stream (e.g., from an .rn2 file).
+     * @return A [Result] containing the parsed [Route] or an exception.
+     */
+    suspend fun loadRoadbook(inputStream: InputStream): Result<Route>
+}

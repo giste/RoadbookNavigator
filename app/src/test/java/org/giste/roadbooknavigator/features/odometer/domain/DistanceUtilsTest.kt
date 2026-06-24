7a813c2d6b977d61d0f47856e32c8179fc7618cd
Giste <giste.trappiste@gmail.com>
2026-06-24 13:49:13 +0200

    test(odometer): add unit tests for DistanceUtils and simple use cases

---
+++ app/src/test/java/org/giste/roadbooknavigator/features/odometer/domain/DistanceUtilsTest.kt
@@ -0,0 +1,99 @@
+/*
+ * Copyright (C) 2026  Giste
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.giste.roadbooknavigator.features.odometer.domain
+
+import org.giste.roadbooknavigator.features.odometer.domain.model.UserLocation
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import kotlin.math.sqrt
+
+class DistanceUtilsTest {
+
+    @Test
+    fun `calculateDistance2D returns zero for same point`() {
+        val loc = createLocation(40.0, -3.0)
+        val distance = DistanceUtils.calculateDistance2D(loc, loc)
+        assertEquals(0.0, distance, 0.001)
+    }
+
+    @Test
+    fun `calculateDistance2D returns correct distance for known points`() {
+        // Points approx 111km apart (1 degree of latitude)
+        val loc1 = createLocation(40.0, -3.0)
+        val loc2 = createLocation(41.0, -3.0)
+        
+        val distance = DistanceUtils.calculateDistance2D(loc1, loc2)
+        
+        // 1 degree of latitude is roughly 111.1 km
+        assertEquals(111194.0, distance, 10.0)
+    }
+
+    @Test
+    fun `calculateDistance uses 3D distance when accuracy is good`() {
+        // Horizontal distance ~3m, Vertical distance 4m -> Result 5m
+        val loc1 = createLocation(40.0, -3.0, altitude = 0.0, verticalAccuracy = 1f)
+        // 0.000027 degrees is approx 3 meters at equator
+        val loc2 = createLocation(40.0, -3.000027, altitude = 4.0, verticalAccuracy = 1f)
+        
+        val horizontal = DistanceUtils.calculateDistance2D(loc1, loc2)
+        val expected3D = sqrt(horizontal * horizontal + 16.0)
+        
+        val distance = DistanceUtils.calculateDistance(loc1, loc2, verticalAccuracyThreshold = 5f)
+        
+        assertEquals(expected3D, distance, 0.001)
+    }
+
+    @Test
+    fun `calculateDistance falls back to 2D when vertical accuracy is poor`() {
+        val loc1 = createLocation(40.0, -3.0, altitude = 0.0, verticalAccuracy = 10f)
+        val loc2 = createLocation(40.1, -3.1, altitude = 100.0, verticalAccuracy = 10f)
+        
+        // Threshold is 5m, accuracy is 10m -> Should use 2D
+        val expected2D = DistanceUtils.calculateDistance2D(loc1, loc2)
+        val distance = DistanceUtils.calculateDistance(loc1, loc2, verticalAccuracyThreshold = 5f)
+        
+        assertEquals(expected2D, distance, 0.001)
+    }
+
+    @Test
+    fun `calculateDistance falls back to 2D when vertical accuracy is null`() {
+        val loc1 = createLocation(40.0, -3.0, altitude = 0.0, verticalAccuracy = null)
+        val loc2 = createLocation(40.1, -3.1, altitude = 100.0, verticalAccuracy = null)
+        
+        val expected2D = DistanceUtils.calculateDistance2D(loc1, loc2)
+        val distance = DistanceUtils.calculateDistance(loc1, loc2)
+        
+        assertEquals(expected2D, distance, 0.001)
+    }
+
+    private fun createLocation(
+        lat: Double,
+        lon: Double,
+        altitude: Double = 0.0,
+        verticalAccuracy: Float? = null
+    ) = UserLocation(
+        latitude = lat,
+        longitude = lon,
+        altitude = altitude,
+        accuracy = 5f,
+        verticalAccuracy = verticalAccuracy,
+        speed = 10f,
+        bearing = 0f,
+        time = 1000L
+    )
+}
---
+++ app/src/test/java/org/giste/roadbooknavigator/features/odometer/domain/usecase/OdometerUseCasesTest.kt
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2026  Giste
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+package org.giste.roadbooknavigator.features.odometer.domain.usecase
+
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
+import org.giste.roadbooknavigator.features.odometer.domain.repository.OdometerRepository
+import org.junit.Before
+import org.junit.Test
+
+class OdometerUseCasesTest {
+
+    private val repository: OdometerRepository = mockk()
+
+    @Before
+    fun setup() {
+        coEvery { repository.updatePartialDistance(any()) } returns Unit
+        coEvery { repository.resetPartialDistance() } returns Unit
+        coEvery { repository.resetAllDistances() } returns Unit
+        coEvery { repository.setPartialDistance(any()) } returns Unit
+    }
+
+    @Test
+    fun `IncrementPartialDistanceUseCase should call repository with +10 meters`() = runTest {
+        val useCase = IncrementPartialDistanceUseCase(repository)
+        useCase()
+        coVerify { repository.updatePartialDistance(10.0) }
+    }
+
+    @Test
+    fun `DecrementPartialDistanceUseCase should call repository with -10 meters`() = runTest {
+        val useCase = DecrementPartialDistanceUseCase(repository)
+        useCase()
+        coVerify { repository.updatePartialDistance(-10.0) }
+    }
+
+    @Test
+    fun `ResetPartialDistanceUseCase should call repository resetPartialDistance`() = runTest {
+        val useCase = ResetPartialDistanceUseCase(repository)
+        useCase()
+        coVerify { repository.resetPartialDistance() }
+    }
+
+    @Test
+    fun `ResetAllDistancesUseCase should call repository resetAllDistances`() = runTest {
+        val useCase = ResetAllDistancesUseCase(repository)
+        useCase()
+        coVerify { repository.resetAllDistances() }
+    }
+
+    @Test
+    fun `SetPartialDistanceUseCase should call repository with specific distance`() = runTest {
+        val useCase = SetPartialDistanceUseCase(repository)
+        val distance = 123.45
+        useCase(distance)
+        coVerify { repository.setPartialDistance(distance) }
+    }
+}

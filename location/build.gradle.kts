f99fa06e46aa24add8ee3bf5eb6f195fa86408f7
Giste <giste.trappiste@gmail.com>
2026-07-10 13:00:37 +0200

    fix: restore androidx.test.core dependency for unit tests in core and location

--- location/build.gradle.kts
+++ location/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     testImplementation(libs.junit)
     testImplementation(libs.mockk)
     testImplementation(libs.kotlinx.coroutines.test)
+    testImplementation(libs.androidx.test.core)
     testImplementation(libs.robolectric)
 
     androidTestImplementation(libs.androidx.junit)
